d7281410ea76d0e3b5d53c5227b89611c9a5e44a
Trevor Johnson <trevor9j@protonmail.com>
2024-03-16 12:51:14 -0700

    finished the SVG class and exported, created inquirer questions

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const { Circle, Square, Triangle } = require('./lib/shapes');
 
+const inquirer = require('inquirer');
+const SVG = require('./lib/svg.js');
+const fs = require('fs');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: "What color do you want your shape to be?",
+        default: 'orange'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape do you want?',
+        choices: ['Square', 'Triangle', 'Circle']
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color do you want the text to be?',
+        default: 'black',
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Add text to your logo!',
+        default: 'SVG',
+    },
+];
